d1d7bba79d5bd0624ca42bd5ed96c0fcce8f385c
mengguiyouziyi <775618369@qq.com>
2017-09-09 15:55:08 +0800

    tyc_jichu_quan

--- baidu/baidu/utils/send.py
+++ baidu/baidu/utils/send.py
@@ -19,11 +19,11 @@ def send_key(key):
 	"""
 		本机 localhost；公司 etl2.innotree.org；服务器 etl1.innotree.org
 	"""
-	mysql = pymysql.connect(host='etl1.innotree.org', port=3308, user='spider', password='spider', db='tyt', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+	mysql = pymysql.connect(host='etl1.innotree.org', port=3308, user='spider', password='spider', db='tyc', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
 	# mysql = pymysql.Connect(host='localhost', user='root', password='3646287', db='spiders', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
 	try:
 		with mysql.cursor() as cursor:
-			sql = """select id, quan_cheng from tyc_jichu_quan ORDER BY ncid desc limit 1000000"""
+			sql = """select id, quan_cheng from tyc_jichu_quan ORDER BY id desc limit 1000000"""
 			print('execute begain')
 			cursor.execute(sql)
 			results = cursor.fetchall()
